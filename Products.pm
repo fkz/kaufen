a94ba8ac6cfa92ae4a6d06bf63096dd29f5203f9
Fabian Schmitthenner <fabian1024@googlemail.com>
2011-10-20 00:42:50 +0200

    E

--- Products.pm
+++ Products.pm
@@ -3,13 +3,85 @@
 use strict;
 use warnings;
 
-
+# ein bestimmtes Produkt
 package Product;
 
+# benannte Produkte
+my %products = ();
+# unbenannte Produkte
+my @products = ();
+
+sub get {
+  my $code = shift;
+  if (!$code) {
+    return new Product;
+  }
+  if ($code =~ /i(\d+)/) {
+    return $products[$1];
+  }
+  else {
+    my $result = $products{$code};
+    $result = new Product $code if (!$result);
+    return $result;
+  }
+}
+
+
+sub list {
+  return (@products, values %products);
+}
+
+sub id {
+  my ($self) = @_;
+  return $self->{id};
+}
+
+sub href {
+  return $_[0]->id;
+}
+
+sub html {
+  my $p = $_[0]->description;
+  return <<HERE
+<?xml version="1.0" encoding="utf8" ?>
+<!DOCTYPE html PUBLIC
+  "-//W3C//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><title>Detail Produkt $p</title></head><body>
+Produkt: $p
+</body>
+</html> 
+HERE
+}
 
 sub new {
-  my $class = shift;
-  return bless {}, $class;
+  my ($class,$id) = @_;
+  my $id2 = $id || ('I' . (scalar @products));
+
+  #check id
+  if ($id) {
+    if ($id !~ /^\d{8,13}$/) {
+      return ("Error", "ungültiger Code");
+    }
+    
+    my @parts = split //, $id;
+    my $sum = 0;
+    my $mul = 3 - 2*(@parts % 2);
+    for (@parts) {
+      $sum += $_ * $mul;
+      $mul = $mul == 1 ? 3 : 1;
+    }
+    if ($sum % 10 != 0) {
+      return ("Error", "Der Code '$id' ist nicht gültig!");
+    }
+  }
+  
+  my $self = bless {id => $id2}, $class;
+  $products{$id} = $self if $id;
+  shift @proucdts, $self unless $id;
+  
+  return bless {id => $id2}, $class;
 }
 
 sub setAttribute {
@@ -27,56 +99,82 @@ sub setGewicht {
   }
 }
 
+sub description {
+  my %ich = %{$_[0]};
+  my @extra = ();
+  for (keys %ich) {
+    next if $_ eq 'id';
+    next if $_ eq 'gewicht';
+    push @extra, $_;
+  }
+  
+  my $extra = join ' ', @extra;
+  
+  if ($ich{gewicht}) {
+    $extra = "Gewicht: $ich{gewicht} $extra";
+  }
+
+  return "Produkt: $ich{id} ($extra)";
+}
+
+sub htmlDescription {
+  my %ich = %{$_[0]};
+  return <<HERE
+<div id="$ich{id}">
+DESCRIPTION OF $ich{id}
+</div>
+HERE
+}
+
 package Item;
 
+my @items = ();
+
 sub new {
-  my $class = shift;
-  my $article = shift;
-  my $price = shift;
-  my $anzahl = shift or 1;
-  my $gewicht = shift;
-  my $self = [ $article, $price, $anzahl, $gewicht ];
-  return bless $self, $class;
+  my ($class, $article, $price, $date, $place, $anzahl, $gewicht) = @_;
+  my $self = [ $article, $price, $date, $place, $anzahl, $gewicht, scalar @items ];
+  $self = bless $self, $class;
+  push @items, $self;
+  return $self;
+}
+
+sub href {
+  return "item_" . $_[0]->[6];
+}
+
+sub html {
+  my @l = @{$_[0]};
+  my $l0href = $l[0]->href;
+  my $l0id = $l[0]->id;
+  return <<HERE
+<?xml version="1.0" encoding="utf8" ?>
+<!DOCTYPE html PUBLIC
+  "-//W3C//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><title>Detail Item $l[6]</title></head><body>
+Produkt: <a href="$l0href">$l0id</a> <br />
+Preis: $l[1]€ <br />
+Kaufdatum: $l[2] <br />
+Einkaufsort: $l[3] <br />
+Anzahl: $l[4] <br />
+</body>
+</html> 
+HERE
+}
+
+sub list {
+  return @items;
+}
+
+sub description {
+  my $self = shift;
+  my $result = $self->[0]->description . "\n";
+  if ($self->[5]) { $result .= "Gewicht: $self->[5]\n" };
+  return $result;
 }
 
 package Products;
 
-
-my %products = ();
-my $unnamedproductcount = 0;
-
-sub insert {
-  my ($class, $code) = @_;
-  
-  my $obj = undef;
-  
-  if ($code) {
-    
-    # check code
-    if ($code !~ /^\d{8,13}$/) {
-      return ("Error", "Der Code $code hat keine gültige Syntax");
-    }
-
-    my @ziffern = split //, $code;
-    my $sum = 0;
-    my $mul = 3 - 2*(@ziffern % 2);
-    for (@ziffern) {
-      $sum += $_ * $mul;
-      $mul = $mul == 1 ? 3 : 1;
-    }
-    if ($sum % 10 != 0) {
-      # error!
-      return ("Error", "Der Code $code ist nicht gültig");
-    }
-    
-    if (!($obj = $products{$code})) {
-      $obj = $products{$code} = new Product;
-    }
-  }
-  else {
-    # maybe, a catching procedure can be found later. For now, just create a new object
-    $obj = $products{"I$unnamedproductcount"} = {};
-  }
-}
 
 1
--- verarbeite.perl
+++ verarbeite.perl
@@ -6,50 +6,15 @@ use warnings;
 use Date::Simple ('date');
 use Products;
 
+sub error {
+  my ($line, $text) = @_;
+  print STDERR "Fehler in Zeile $line: $text\n";
+}
+
 # Zeichen, die in einem Begriff auftreten dürfen
 my $w = '[\wäöüÄÖÜ,!€\.%]+';
 
 my %actual = ();
-
-my @articles = ();
-my %products = ();
-
-my %names = ();
-my @names = ();
-
-sub get {
-  my ($word) = @_;
-  my $id = $names{$word};
-  
-  if (defined $id) { return $id; }
-
-  push @names, ($word);
-  $names{$word} = $#names;
-  
-  return $#names;
-}
-
-sub synonym {
-  
-  my ($A, $B) = @_;
-
-  if (defined $names{$A} && defined $names{$B}) {
-    warn "$A und $B wurden beide bereits benutzt; kann nicht identisch machen!";
-    return;
-  }
-  
-  (!(defined $names{$A} || defined $names{$B})) and get $A;
-  my $v = $names{$A} || $names{$B};
-  $names{$A} = $names{$B} = $v;
-  return $v;   
-}
-
-sub kategorie {
-  my ($A, $B) = @_;
-  my $a = get $A;
-  my $b = get $B;
-  #@TODO
-}
 
 my $line = 0;
 while (<>) {
@@ -58,30 +23,29 @@ while (<>) {
     if ($2 eq ':=')  {
       my $first = $1;
       my $second = $3;
-      ($first !~ /^(Datum|Geschäft|Ort)$/) and warn "$first ist nicht definiert";
+      ($first !~ /^(Datum|Geschäft|Ort)$/) and error $line, "$first ist nicht definiert";
       if ($first eq 'Datum') {
 	#parse date $second
 	if ($second =~ /^(\d+)\.(\d+)\.(\d+)$/) {
 	  $actual{$first} = date ($3 < 100 ? $3 + 2000 : $3, $2, $1);
 	}
 	else {
-	  warn "$second hat kein gültiges Datumsformat";
+	  error $line, "Kein gültiges Datumsformat";
 	}
       }
       else {
-	$actual{$first} = get $second;
+	$actual{$first} = $second;
       }
     }
-    synonym $3, $1 if $2 eq '=';
-    kategorie $3, $1 if $2 eq '~';
+    #synonym $3, $1 if $2 eq '=';
+    #kategorie $3, $1 if $2 eq '~';
   }
   elsif (/^\s*(($w\s+)*$w)\s*$/) {
     my @words = split /\s+/;
+    
     my %article = (
-      Datum => $actual{Datum},
       Ort => $actual{Ort},
-      'Geschäft' => $actual{'Geschäft'},
-      Properties => {}
+      'Geschäft' => $actual{'Geschäft'}
     );
     
     my %props = ();
@@ -94,43 +58,59 @@ while (<>) {
       elsif (/^(\d+,?\d*)(kg|g)$/) {
 	$article{Gewicht} = $1;
 	$article{Gewicht} =~ s/,/\./;
-	$2 eq 'g' and $article{Gewicht} /= 1000;
+	($2 eq 'g') and ($article{Gewicht} /= 1000);
       }
       elsif (/^\d{5,13}$/) {
 	$article{Code} = $_;
-	#Prüfziffer berechnen:
-	my @ziffern = split //;
-	my $sum = 0;
-	my $mul = 3 - 2*(@ziffern % 2);
-	for (@ziffern) {
-	  $sum += $_ * $mul;
-	  $mul = $mul == 1 ? 3 : 1;
-	}
-	if ($sum % 10 != 0) {
-	  warn "Prüfziffer falsch $_";
-	}
       }
       else {
-	$article{Properties}->{get $_} = 1;
+	$article{Properties}->{$_} = 1;
       }
     }
     
-    push @articles, \%article
+    my $product = Product::get $article{Code};
+    $product->setGewicht ($article{Gewicht}) if $article{Gewicht};
+    for (keys %{$article{Properties}}) {
+      $product->setAttribute ($_);
+    }
+    
+    new Item ($product, $article{Preis}, $actual{Datum}, $actual{Ort}, 1, undef);
   }
   else {
-    /^\s*$/ or warn "$_ wurde nicht verarbeitet!";
+    /^\s*$/ or error $line, "$_ wurde nicht verarbeitet!";
   }
 }
+
+# erzeuge HTML-Dateien
+
+mkdir "html";
+chdir "html";
+
+
+for (Item::list) {
+  open my $fh, ">", $_->href;
+   print $fh $_->html;
+}
+
+for (Product::list) {
+  open my $fh, ">", $_->href;
+  print $fh $_->html;
+}
+
 
 my $Preis = 0;
 
-for (@articles) {
-  my %article = %$_;
-  my %attrs = %{$article{Properties}};
-  print "Artikel: $article{Code} (", (join ' ', keys %attrs), ")\n";
-  print "  Preis: $article{Preis}€\n";
-  print "  Gewicht: $article{Gewicht}kg\n";
-  $Preis += $article{Preis};
+for (Item::list) {
+  print $_->description;
 }
 
-print STDERR scalar @articles, " Produkte verarbeitet im Gesamtwert von $Preis€\n";
+#for (@articles) {
+#  my %article = %$_;
+#  my %attrs = %{$article{Properties}};
+#  print "Artikel: $article{Code} (", (join ' ', keys %attrs), ")\n";
+#  print "  Preis: $article{Preis}€\n";
+#  print "  Gewicht: $article{Gewicht}kg\n";
+#  $Preis += $article{Preis};
+#}
+
+#print STDERR scalar @articles, " Produkte verarbeitet im Gesamtwert von $Preis€\n";
